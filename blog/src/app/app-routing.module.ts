a3c0d34273fe64f23d9c2e3a9e269ece5e8492f0
Mustafa <mustafa.ter@hotmail.com>
2020-12-03 15:47:38 +0300

    nav bar oluşturuldu

--- blog/src/app/app-routing.module.ts
+++ blog/src/app/app-routing.module.ts
@@ -1,7 +1,45 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactComponent } from './contact/contact.component';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  // main route
+  {
+    path:"",
+    component:MainLayoutComponent,
+    children:[
+      {
+        path:"",
+        component:HomeComponent
+      },
+      {
+        path:"home",
+        component:HomeComponent
+      },
+      {
+        path:"hakkimda",
+        component:AboutMeComponent
+      },
+      {
+        //www.domain.com/iletisim
+        path:"iletisim",
+        component:ContactComponent
+      }
+
+    ]
+  },
+  {
+    path:"admin",
+    component:AdminLayoutComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ blog/src/app/nav/main-nav/main-nav.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import {Router,NavigationEnd} from "@angular/router";
+enum MainPagies{
+  home=1,
+  about_me=2,
+  contact=3
+}
+
+@Component({
+  selector: 'app-main-nav',
+  templateUrl: './main-nav.component.html',
+  styleUrls: ['./main-nav.component.css']
+})
+export class MainNavComponent implements OnInit {
+  pageActive: MainPagies = 1;
+  constructor(private router: Router) {
+    this.router.events.subscribe(x=>{
+      if(x instanceof NavigationEnd)
+      {
+        if(x.url.indexOf("home")>0)
+        {
+          this.pageActive=MainPagies.home
+        }
+        else if(x.url.indexOf("hakkimda")>0)
+        {
+          this.pageActive=MainPagies.about_me
+        }
+        else if(x.url.indexOf("iletisim")>0)
+        {
+          this.pageActive=MainPagies.contact
+        }
+        else
+        {
+          this.pageActive=MainPagies.home
+        }
+      }
+
+    })
+
+  }
+
+  ngOnInit(): void {
+  }
+
+}
